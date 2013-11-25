4a6c7a0e2bc69ed629a55b222b2c352afdf9c875
Shawn <calltherain@gmail.com>
2013-11-26 02:38:03 +0800

    First checkin

---
+++ firmwares/cvpJoystickTest/Arduino-big-joystick.c
@@ -0,0 +1,322 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/*-
+ * Copyright (c) 2011 Darran Hunt (darran [at] hunt dot net dot nz)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** \file
+ *
+ *  Main source file for the Arduino-mouse project. This file contains the main tasks of
+ *  the project and is responsible for the initial application hardware configuration.
+ */
+
+#include "Arduino-joystick.h"
+
+/** Buffer to hold the previously generated HID report, for comparison purposes inside the HID class driver. */
+uint8_t PrevJoystick1HIDReportBuffer[sizeof(USB_Joystick1Report_Data_t)];
+uint8_t PrevJoystick2HIDReportBuffer[sizeof(USB_Joystick2Report_Data_t)];
+
+/** LUFA HID Class driver interface configuration and state information. This structure is
+ *  passed to all HID Class driver functions, so that multiple instances of the same class
+ *  within a device can be differentiated from one another.
+ */
+USB_ClassInfo_HID_Device_t Joystick1_HID_Interface = {
+    .Config = {
+	.InterfaceNumber              = 0,
+
+	.ReportINEndpointNumber = JOYSTICK1_EPNUM,
+	.ReportINEndpointSize = JOYSTICK_EPSIZE,
+	.ReportINEndpointDoubleBank = false,
+
+	.PrevReportINBuffer           = PrevJoystick1HIDReportBuffer,
+	.PrevReportINBufferSize       = sizeof(PrevJoystick1HIDReportBuffer),
+    },
+};
+
+USB_ClassInfo_HID_Device_t Joystick2_HID_Interface = {
+    .Config = {
+	.InterfaceNumber              = 1,
+
+	.ReportINEndpointNumber = JOYSTICK2_EPNUM,
+	.ReportINEndpointSize = JOYSTICK_EPSIZE,
+	.ReportINEndpointDoubleBank = false,
+
+	.PrevReportINBuffer           = PrevJoystick1HIDReportBuffer,
+	.PrevReportINBufferSize       = sizeof(PrevJoystick1HIDReportBuffer),
+    },
+};
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+
+/** Circular buffer to hold data from the serial port before it is sent to the host. */
+RingBuffer_t USARTtoUSB_Buffer;
+uint8_t USARTtoUSB_BufferStorage[ JOYSTICK_EPSIZE * 5 ];
+
+USB_Joystick1Report_Data_t joy1Report;
+USB_Joystick2Report_Data_t joy2Report;
+
+uint16_t joy1DataSize = sizeof(USB_Joystick1Report_Data_t);
+uint16_t joy2DataSize = sizeof(USB_Joystick2Report_Data_t);
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+int main(void)
+{
+	SetupHardware();
+
+	RingBuffer_InitBuffer(&USARTtoUSB_Buffer, USARTtoUSB_BufferStorage, sizeof( USARTtoUSB_BufferStorage) );
+
+    	sei();
+
+    	for (;;) {
+		Joystick1_Task();
+		Joystick2_Task();
+		USB_USBTask();
+	}
+}
+
+/** Configures the board hardware and chip peripherals for the demo's functionality. */
+void SetupHardware(void)
+{
+    /* Disable watchdog if enabled by bootloader/fuses */
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
+
+    /* Hardware Initialization */
+    Serial_Init(115200, true);
+    LEDs_Init();
+    USB_Init();
+
+    UCSR1B = ((1 << RXCIE1) | (1 << TXEN1) | (1 << RXEN1));
+}
+
+/** Event handler for the library USB Connection event. */
+void EVENT_USB_Device_Connect(void)
+{
+	//LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+}
+
+/** Event handler for the library USB Disconnection event. */
+void EVENT_USB_Device_Disconnect(void)
+{
+}
+
+/** Event handler for the library USB Configuration Changed event. */
+void EVENT_USB_Device_ConfigurationChanged(void)
+{
+     Endpoint_ConfigureEndpoint(JOYSTICK1_EPNUM, EP_TYPE_INTERRUPT, ENDPOINT_DIR_IN,
+                                                   JOYSTICK_EPSIZE, ENDPOINT_BANK_SINGLE);
+
+     Endpoint_ConfigureEndpoint(JOYSTICK2_EPNUM, EP_TYPE_INTERRUPT, ENDPOINT_DIR_IN,
+                                                   JOYSTICK_EPSIZE, ENDPOINT_BANK_SINGLE);
+
+
+//    HID_Device_ConfigureEndpoints(&Joystick1_HID_Interface);
+//    HID_Device_ConfigureEndpoints(&Joystick2_HID_Interface);
+//
+    USB_Device_EnableSOFEvents();
+}
+
+/** Event handler for the library USB Unhandled Control Request event. */
+void EVENT_USB_Device_UnhandledControlRequest(void)
+{
+    HID_Device_ProcessControlRequest(&Joystick1_HID_Interface);
+    HID_Device_ProcessControlRequest(&Joystick2_HID_Interface);
+}
+
+/** Event handler for the USB device Start Of Frame event. */
+void EVENT_USB_Device_StartOfFrame(void)
+{
+    HID_Device_MillisecondElapsed(&Joystick1_HID_Interface);
+    HID_Device_MillisecondElapsed(&Joystick2_HID_Interface);
+}
+
+
+void EVENT_USB_Device_ControlRequest(void)
+{
+        uint8_t* ReportData;
+        uint8_t  ReportSize;
+
+        /* Handle HID Class specific requests */
+
+        switch (USB_ControlRequest.bRequest)
+        {
+	case HID_REQ_GetReport:
+	        if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+	        {
+	                Endpoint_ClearSETUP();
+	
+	                /* Determine if it is the joystick1 or 2 data that is being requested */
+	                if (USB_ControlRequest.wIndex==0)
+	                {
+	                        ReportData = (uint8_t*)&joy1Report;
+	                        ReportSize = sizeof(joy1Report);
+	                }
+	                else
+	                {
+	                        ReportData = (uint8_t*)&joy2Report;
+	                        ReportSize = sizeof(joy2Report);
+	                }
+	
+	                /* Write the report data to the control endpoint */
+	                Endpoint_Write_Control_Stream_LE(ReportData, ReportSize);
+	                Endpoint_ClearOUT();
+	
+			//keep the report data so it can be reused.
+	        }
+	        break;
+       }
+}
+
+// get the data from ringbuffer and save it to report buffer
+void PrepareReportData(void)
+{
+	//check the ring buffer data size
+	uint16_t BufferCount = RingBuffer_GetCount(&USARTtoUSB_Buffer);
+	/* If there's a new report from the Arduino, copy it to local variable for future use 
+	 If not then the last report will be reused
+	*/
+	if (BufferCount >= ( joy1DataSize + joy2DataSize ) ) {
+		uint16_t ind;
+		for (ind=0; ind < joy1DataSize; ind++) {
+		    ((uint8_t *)&joy1Report)[ind] = RingBuffer_Remove(&USARTtoUSB_Buffer);
+		}
+		for (ind=0; ind < joy2DataSize; ind++) {
+		    ((uint8_t *)&joy2Report)[ind] = RingBuffer_Remove(&USARTtoUSB_Buffer);
+		}
+	}
+}
+
+void Joystick1_Task(void)
+{
+        /* Device must be connected and configured for the task to run */
+        if (USB_DeviceState != DEVICE_STATE_Configured)
+          return;
+
+        //prepare the data
+	PrepareReportData();
+
+        /* Select the Mouse Report Endpoint */
+        Endpoint_SelectEndpoint(JOYSTICK1_EPNUM);
+
+        /* Check if Mouse Endpoint Ready for Read/Write */
+        if (Endpoint_IsReadWriteAllowed())
+        {
+                /* Write Mouse Report Data */
+                Endpoint_Write_Stream_LE(&joy1Report, joy1DataSize, NULL);
+
+                /* Finalize the stream transfer to send the last packet */
+                Endpoint_ClearIN();
+
+		//just keep the report data so it can be reused, 
+        }
+}
+
+void Joystick2_Task(void)
+{
+        /* Device must be connected and configured for the task to run */
+        if (USB_DeviceState != DEVICE_STATE_Configured)
+          return;
+
+        //prepare the data
+	PrepareReportData();
+
+        /* Select the Mouse Report Endpoint */
+        Endpoint_SelectEndpoint(JOYSTICK2_EPNUM);
+
+        /* Check if Mouse Endpoint Ready for Read/Write */
+        if (Endpoint_IsReadWriteAllowed())
+        {
+                /* Write Mouse Report Data */
+                Endpoint_Write_Stream_LE(&joy2Report, joy2DataSize, NULL);
+
+                /* Finalize the stream transfer to send the last packet */
+                Endpoint_ClearIN();
+
+		//we just keep the report data so it can be reused, 
+        }
+}
+
+
+
+/** HID class driver callback function for the processing of HID reports from the host.
+ *
+ *  \param[in] HIDInterfaceInfo  Pointer to the HID class interface configuration structure being referenced
+ *  \param[in] ReportID    Report ID of the received report from the host
+ *  \param[in] ReportType  The type of report that the host has sent, either REPORT_ITEM_TYPE_Out or REPORT_ITEM_TYPE_Feature
+ *  \param[in] ReportData  Pointer to a buffer where the created report has been stored
+ *  \param[in] ReportSize  Size in bytes of the received HID report
+ */
+void CALLBACK_HID_Device_ProcessHIDReport(USB_ClassInfo_HID_Device_t* const HIDInterfaceInfo,
+                                          const uint8_t ReportID,
+                                          const uint8_t ReportType,
+                                          const void* ReportData,
+                                          const uint16_t ReportSize)
+{
+    /* Not used but must be present */
+}
+
+/** ISR to manage the reception of data from the serial port, placing received bytes into a circular buffer
+ *  for later transmission to the host.
+ */
+ISR(USART1_RX_vect, ISR_BLOCK)
+{
+    uint8_t ReceivedByte = UDR1;
+
+    if ( (USB_DeviceState == DEVICE_STATE_Configured) && !RingBuffer_IsFull(&USARTtoUSB_Buffer) ) 
+    {
+	RingBuffer_Insert(&USARTtoUSB_Buffer, ReceivedByte);
+    }
+}
---
+++ firmwares/cvpJoystickTest/Descriptors.c
@@ -0,0 +1,357 @@
+/*
+             LUFA Library
+     Copyright (C) Dean Camera, 2010.
+              
+  dean [at] fourwalledcubicle [dot] com
+      www.fourwalledcubicle.com
+*/
+
+/*
+  Copyright 2010  Dean Camera (dean [at] fourwalledcubicle [dot] com)
+
+  Permission to use, copy, modify, distribute, and sell this 
+  software and its documentation for any purpose is hereby granted
+  without fee, provided that the above copyright notice appear in 
+  all copies and that both that the copyright notice and this
+  permission notice and warranty disclaimer appear in supporting 
+  documentation, and that the name of the author not be used in 
+  advertising or publicity pertaining to distribution of the 
+  software without specific, written prior permission.
+
+  The author disclaim all warranties with regard to this
+  software, including all implied warranties of merchantability
+  and fitness.  In no event shall the author be liable for any
+  special, indirect or consequential damages or any damages
+  whatsoever resulting from loss of use, data or profits, whether
+  in an action of contract, negligence or other tortious action,
+  arising out of or in connection with the use or performance of
+  this software.
+*/
+
+/** \file
+ *
+ *  USB Device Descriptors, for library use when in USB device mode. Descriptors are special 
+ *  computer-readable structures which the host requests upon device enumeration, to determine
+ *  the device's capabilities and functions.  
+ */
+
+#include "Descriptors.h"
+
+/** HID class report descriptor. This is a special descriptor constructed with values from the
+ *  USBIF HID class specification to describe the reports and capabilities of the HID device. This
+ *  descriptor is parsed by the host and its contents used to determine what data (and in what encoding)
+ *  the device will send, and what it may be sent back from the host. Refer to the HID specification for
+ *  more details on HID report descriptors.
+ */
+const USB_Descriptor_HIDReport_Datatype_t PROGMEM Joystick1Report[] =
+{
+	0x05, 0x01,          /* Usage Page (Generic Desktop)                       */
+	0x09, 0x04,          /* Usage (Joystick)                                   */
+
+	0xa1, 0x01,          /* Collection (Application)                           */
+	0x09, 0x01,          /*   Usage (Pointer)                                  */
+
+	/* 8 axes, signed 16 bit resolution, range -32768 to 32767 (16 bytes) */
+	0xa1, 0x00,          /*   Collection (Physical)                            */
+	0x05, 0x01,          /*     Usage Page (Generic Desktop)                   */
+	0x09, 0x30,          /*     Usage (X)                                      */
+	0x09, 0x31,          /*     Usage (Y)                                      */
+	0x09, 0x32,          /*     Usage (Analog1)                                */
+	0x09, 0x33,          /*     Usage (Analog2)                                */
+	0x09, 0x34,          /*     Usage (Analog3)                                */
+	0x09, 0x35,          /*     Usage (Analog4)                                */
+	0x09, 0x36,          /*     Usage (Analog5)                                */
+	0x09, 0x37,          /*     Usage (Analog6)                                */
+	0x27, 0xff, 0xff, 0x00, 0x00,    /*     Logical Maximum (65535)                        */
+	0x47, 0xff, 0xff, 0x00, 0x00,    /*     Physical Maximum (65535)                        */
+	0x75, 16,            /*     Report Size (16)                               */
+	0x95, 8,             /*     Report Count (8)                               */
+	0x81, 0x82,          /*     Input (Data, Variable, Absolute, Volatile)     */
+	0xc0,                /*   End Collection                                   */
+
+	/* 248 buttons, value 0=off, 1=on (6 bytes) */
+	0x05, 0x09,          /*   Usage Page (Button)                              */
+	0x19, 1,             /*     Usage Minimum (Button 1)                       */
+	0x29, 128,            /*     Usage Maximum (Button 128 )                      */
+	0x15, 0x00,          /*   Logical Minimum (0)                              */
+	0x25, 0x01,          /*   Logical Maximum (1)                              */
+	0x35, 0x00,          /*   Physical Minimum (0)                              */
+	0x45, 0x01,          /*   Physical Maximum (1)                              */
+	0x75, 1,             /*   Report Size (1)                                  */
+	0x95, 128,            /*   Report Count (128)                                */
+	0x81, 0x02,          /*   Input (Data, Variable, Absolute)                 */
+	0xc0                 /* End Collection                                     */
+};
+
+const USB_Descriptor_HIDReport_Datatype_t PROGMEM Joystick2Report[] =
+{
+	0x05, 0x01,          /* Usage Page (Generic Desktop)                       */
+	0x09, 0x04,          /* Usage (Joystick)                                   */
+
+	0xa1, 0x01,          /* Collection (Application)                           */
+
+	0x05, 0x09,          /*   Usage Page (Button)                              */
+	0x19, 1,             /*     Usage Minimum (Button 1)                       */
+	0x29, 128,            /*     Usage Maximum (Button 128 )                      */
+	0x15, 0x00,          /*   Logical Minimum (0)                              */
+	0x25, 0x01,          /*   Logical Maximum (1)                              */
+	0x35, 0x00,          /*   Physical Minimum (0)                              */
+	0x45, 0x01,          /*   Physical Maximum (1)                              */
+	0x75, 1,             /*   Report Size (1)                                  */
+	0x95, 128,            /*   Report Count (128)                                */
+	0x81, 0x02,          /*   Input (Data, Variable, Absolute)                 */
+	0xc0                 /* End Collection                                     */
+};
+
+/** Device descriptor structure. This descriptor, located in FLASH memory, describes the overall
+ *  device characteristics, including the supported USB version, control endpoint size and the
+ *  number of device configurations. The descriptor is read out by the USB host when the enumeration
+ *  process begins.
+ */
+const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
+{
+	.Header                 = {.Size = sizeof(USB_Descriptor_Device_t), .Type = DTYPE_Device},
+		
+	.USBSpecification       = VERSION_BCD(02.00),
+	.Class                  = 0x00,
+	.SubClass               = 0x00,
+	.Protocol               = 0x00,
+	
+	.Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
+		
+	.VendorID               = 0x03EB,
+	.ProductID              = 0x2043,
+	.ReleaseNumber          = 0x0003,
+		
+	.ManufacturerStrIndex   = 0x01,
+	.ProductStrIndex        = 0x02,
+	//.SerialNumStrIndex      = NO_DESCRIPTOR,
+	.SerialNumStrIndex      = 0x03,
+		
+	.NumberOfConfigurations = FIXED_NUM_CONFIGURATIONS
+};
+
+/** Configuration descriptor structure. This descriptor, located in FLASH memory, describes the usage
+ *  of the device in one of its supported configurations, including information about any device interfaces
+ *  and endpoints. The descriptor is read out by the USB host during the enumeration process when selecting
+ *  a configuration so that the host may correctly communicate with the USB device.
+ */
+const USB_Descriptor_Configuration_t PROGMEM ConfigurationDescriptor =
+{
+	.Config = 
+	{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Configuration_Header_t), .Type = DTYPE_Configuration},
+
+			.TotalConfigurationSize = sizeof(USB_Descriptor_Configuration_t),
+			.TotalInterfaces        = 2,
+				
+			.ConfigurationNumber    = 1,
+			.ConfigurationStrIndex  = NO_DESCRIPTOR,
+				
+			.ConfigAttributes       = (USB_CONFIG_ATTR_BUSPOWERED ),
+			
+			.MaxPowerConsumption    = USB_CONFIG_POWER_MA(100)
+	},
+		
+	.HID_Interface1 = 
+	{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Interface_t), .Type = DTYPE_Interface},
+
+			.InterfaceNumber        = 0x00,
+			.AlternateSetting       = 0x00,
+			
+			.TotalEndpoints         = 1,
+				
+			.Class                  = 0x03,
+			.SubClass               = 0x00,
+			.Protocol               = HID_CSCP_NonBootProtocol,
+				
+			.InterfaceStrIndex      = NO_DESCRIPTOR
+	},
+
+	.HID_Joystick1HID = 
+	{
+			.Header                 = {.Size = sizeof(USB_HID_Descriptor_HID_t), .Type = HID_DTYPE_HID},
+			.HIDSpec                = VERSION_BCD(01.11),
+			.CountryCode            = 0x00,
+			.TotalReportDescriptors = 1,
+			.HIDReportType          = HID_DTYPE_Report,
+			.HIDReportLength        = sizeof(Joystick1Report)
+	},
+
+	.HID_J1_ReportINEndpoint = 
+	{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
+			
+			.EndpointAddress        = (ENDPOINT_DIR_IN | JOYSTICK1_EPNUM),
+			.Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+			.EndpointSize           = JOYSTICK_EPSIZE,
+			.PollingIntervalMS      = 0x05
+	},
+
+	.HID_Interface2 = 
+	{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Interface_t), .Type = DTYPE_Interface},
+
+			.InterfaceNumber        = 0x01,
+			.AlternateSetting       = 0x00,
+			
+			.TotalEndpoints         = 1,
+				
+			.Class                  = 0x03,
+			.SubClass               = 0x00,
+			.Protocol               = HID_CSCP_NonBootProtocol,
+				
+			.InterfaceStrIndex      = NO_DESCRIPTOR
+	},
+
+	.HID_Joystick2HID = 
+	{
+			.Header                 = {.Size = sizeof(USB_HID_Descriptor_HID_t), .Type = HID_DTYPE_HID},
+			.HIDSpec                = VERSION_BCD(01.11),
+			.CountryCode            = 0x00,
+			.TotalReportDescriptors = 1,
+			.HIDReportType          = HID_DTYPE_Report,
+			.HIDReportLength        = sizeof(Joystick2Report)
+	},
+
+	.HID_J2_ReportINEndpoint = 
+	{
+			.Header                 = {.Size = sizeof(USB_Descriptor_Endpoint_t), .Type = DTYPE_Endpoint},
+			
+			.EndpointAddress        = (ENDPOINT_DIR_IN | JOYSTICK2_EPNUM),
+			.Attributes             = (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+			.EndpointSize           = JOYSTICK_EPSIZE,
+			.PollingIntervalMS      = 0x05
+	},
+
+};
+
+/** Language descriptor structure. This descriptor, located in FLASH memory, is returned when the host requests
+ *  the string descriptor with index 0 (the first index). It is actually an array of 16-bit integers, which indicate
+ *  via the language ID table available at USB.org what languages the device supports for its string descriptors.
+ */
+const USB_Descriptor_String_t PROGMEM LanguageString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(1), .Type = DTYPE_String},
+		
+	.UnicodeString          = {LANGUAGE_ID_ENG}
+};
+
+/** Manufacturer descriptor string. This is a Unicode string containing the manufacturer's details in human readable
+ *  form, and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
+ *  Descriptor.
+ */
+const USB_Descriptor_String_t PROGMEM ManufacturerString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(7), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"Arduino"
+};
+
+/** Product descriptor string. This is a Unicode string containing the product's details in human readable form,
+ *  and is read out upon request by the host when the appropriate string ID is requested, listed in the Device
+ *  Descriptor.
+ */
+const USB_Descriptor_String_t PROGMEM ProductString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(25), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"C.V.P Joystick Controller"
+};
+
+/* Serial Number */
+const USB_Descriptor_String_t PROGMEM SerialNumberString =
+{
+	.Header                 = {.Size = USB_STRING_LEN(20), .Type = DTYPE_String},
+		
+	.UnicodeString          = L"Composite Controller"
+};
+/** This function is called by the library when in device mode, and must be overridden (see library "USB Descriptors"
+ *  documentation) by the application code so that the address and size of a requested descriptor can be given
+ *  to the USB library. When the device receives a Get Descriptor request on the control endpoint, this function
+ *  is called so that the descriptor details can be passed back and the appropriate descriptor sent back to the
+ *  USB host.
+ */
+uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
+                                    const uint8_t wIndex,
+                                    const void** const DescriptorAddress)
+{
+	const uint8_t  DescriptorType   = (wValue >> 8);
+	const uint8_t  DescriptorNumber = (wValue & 0xFF);
+
+	void*    Address = NULL;
+	uint16_t Size    = NO_DESCRIPTOR;
+
+	switch (DescriptorType)
+	{
+		case DTYPE_Device:
+			Address = &DeviceDescriptor;
+			Size    = sizeof(USB_Descriptor_Device_t);
+			break;
+		case DTYPE_Configuration: 
+			Address = &ConfigurationDescriptor;
+			Size    = sizeof(USB_Descriptor_Configuration_t);
+			break;
+		case DTYPE_String: 
+			switch (DescriptorNumber)
+			{
+				case 0x00: 
+					Address = &LanguageString;
+					Size    = pgm_read_byte(&LanguageString.Header.Size);
+					break;
+				case 0x01: 
+					Address = &ManufacturerString;
+					Size    = pgm_read_byte(&ManufacturerString.Header.Size);
+					break;
+				case 0x02: 
+					Address = &ProductString;
+					Size    = pgm_read_byte(&ProductString.Header.Size);
+					break;
+				case 0x03:
+					Address = &SerialNumberString;
+					Size 	= pgm_read_byte(&SerialNumberString.Header.Size);
+					break;
+			}
+			
+			break;
+		case HID_DTYPE_HID: 
+			//switch( wIndex )
+			//{
+			//	case 0x00:
+			if ( wIndex == 0 )
+			{
+				Address = (void*)&ConfigurationDescriptor.HID_Joystick1HID;
+				Size    = sizeof(USB_HID_Descriptor_HID_t);
+				break;
+			}
+			else
+			{
+			//case 0x01:
+				Address = (void*)&ConfigurationDescriptor.HID_Joystick2HID;
+				Size    = sizeof(USB_HID_Descriptor_HID_t);
+				break;
+			}
+			//}
+			break;
+		case HID_DTYPE_Report: 
+			//switch( wIndex )
+			//{
+			//	case 0x00:
+			if( wIndex == 0 )
+			{
+					Address = (void*)&Joystick1Report;
+					Size    = sizeof(Joystick1Report);
+					break;
+			}else
+			{
+			//	case 0x01:
+					Address = (void*)&Joystick2Report;
+					Size    = sizeof(Joystick2Report);
+					break;
+			}
+			break;
+	}
+	*DescriptorAddress = Address;
+	return Size;
+}
